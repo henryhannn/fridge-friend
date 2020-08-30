1818d57c2ba32d36b2f5aea1b50e1462d29663e1
henryhannn <henryhan3996@gmail.com>
2020-08-30 16:57:40 -0700

    setup mongodb

--- app.js
+++ app.js
@@ -1,7 +1,20 @@
+const mongoose = require('mongoose');
+const users = require("./routes/api/users");
 const express = require("express");
 const app = express();
+const db = require('./config/keys').mongoURI;
+const bodyParser = require('body-parser');
 
-app.get("/", (req, res) => res.send("NO DEMONS"));
+mongoose
+  .connect(db, { useNewUrlParser: true })
+  .then(() => console.log("Connected to MongoDB successfully"))
+  .catch(err => console.log(err));
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.get("/", (req, res) => res.send("FOR THE HUNGRY ROOMMATE"));
+app.use("/api/users", users);
+
 const port = process.env.PORT || 5000;
-
 app.listen(port, () => console.log(`Server is running on port ${port}`));
+
