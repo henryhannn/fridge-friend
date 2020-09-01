460f110fc015af44947e232929eac77d0e9afca6
henryhannn <henryhan3996@gmail.com>
2020-09-01 15:52:17 -0700

    setup for heroku

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const app = express();
 const db = require('./config/keys').mongoURI;
 const bodyParser = require('body-parser');
 const passport = require('passport');
+const path = require('path');
 
 mongoose
   .connect(db, { useNewUrlParser: true })
@@ -24,3 +25,9 @@ app.use("/api/fridges", fridges);
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Server is running on port ${port}`));
 
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('frontend/build'));
+  app.get('/', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
+  })
+}
