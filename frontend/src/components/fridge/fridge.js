ca5581cf7fcb0b686c78d8f3fbeec7b93f6b32c7
anyahirota <anyahirota@gmail.com>
2020-09-02 21:28:19 -0700

    Start expiration tracking functionality

--- frontend/src/components/fridge/fridge.js
+++ frontend/src/components/fridge/fridge.js
@@ -16,6 +16,22 @@ class Fridge extends React.Component {
     this.props.history.push("/foods");
   }
 
+  daysUntilExp() {
+    const today = new Date();
+    const dd = String(today.getDate()).padStart(2, "0");
+    const mm = String(today.getMonth() + 1).padStart(2, "0"); 
+    const yyyy = today.getFullYear();
+
+    const currentDate = yyyy + "-" + mm + "-" + dd;
+    const expDate = this.props.expirationDate; // get item expiration date
+
+    const diffDays = (expirationDate, currentDate) =>
+      Math.ceil((expirationDate - currentDate) / (1000 * 60 * 60 * 24));
+
+    //will return the num of days until expiration as pos int, and days passed expiration as neg int
+    return diffDays(new Date(expDate), new Date(currentDate));  
+  }
+
   expiringItemsColor() {
     // this function will change the text-color based on how close the expiration date is:
     // 1 week or more $dark-blue
