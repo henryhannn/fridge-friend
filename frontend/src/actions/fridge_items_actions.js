aa3bcca325c0f56ad188f27a793330594f2bc40b
anyahirota <anyahirota@gmail.com>
2020-09-03 14:15:48 -0700

    Add error message to form

--- frontend/src/actions/fridge_items_actions.js
+++ frontend/src/actions/fridge_items_actions.js
@@ -1,11 +1,19 @@
 import * as fridgeItemAPIUtil from '../util/fridge_item_util';
 
 export const RECEIVE_FRIDGE_ITEMS = "RECEIVE_FRIDGE_ITEMS";
+export const RECEIVE_FRIDGE_ITEMS_ERRORS = "RECEIVE_FRIDGE_ITEMS_ERRORS";
 
 const receiveFridgeItems = (fridgeItems) => ({
   type: RECEIVE_FRIDGE_ITEMS,
   fridgeItems
 });
+
+export const receiveErrors = (errors) => {
+  
+  return {
+  type: RECEIVE_FRIDGE_ITEMS_ERRORS,
+  errors,
+}};
 
 export const requestFridgeItems = (fridgeId) => (dispatch) =>
   fridgeItemAPIUtil.getFridgeItems(fridgeId)
@@ -17,8 +25,12 @@ export const removeFridgeItem = (fridgeId, fridgeItemId) => (dispatch) =>
 
 // itemData should contain keys name, category, owner, quantity, expirationDate and imageUrl
 export const addFridgeItem = (fridgeId, itemData) => (dispatch) =>
-  fridgeItemAPIUtil.editFridgeItem(fridgeId, itemData)
-    .then((fridgeItems) => dispatch(receiveFridgeItems(fridgeItems.data)));
+  fridgeItemAPIUtil
+    .editFridgeItem(fridgeId, itemData)
+    .then((fridgeItems) => dispatch(receiveFridgeItems(fridgeItems.data)))
+    .catch((err) => {
+      dispatch(receiveErrors(err.response.data));
+    });
 
 //itemData should contain keys fridgeItemId and quantity
 export const editFridgeItemQuantity = (fridgeId, itemData) => (dispatch) =>
--- frontend/src/reducers/errors_reducer.js
+++ frontend/src/reducers/errors_reducer.js
@@ -1,7 +1,11 @@
 import { combineReducers } from "redux";
 
 import SessionErrorsReducer from "./session_errors_reducer";
+import FridgeItemsErrorsReducer from "./fridge_items_errors_reducer"; 
+
 
 export default combineReducers({
   session: SessionErrorsReducer,
+  fridgeItems: FridgeItemsErrorsReducer,
+  
 });
