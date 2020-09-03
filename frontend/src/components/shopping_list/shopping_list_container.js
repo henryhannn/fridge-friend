97cb75614ea776c61e6e822cb4209a6d83305cc2
Jack Fragassi <jfragassi98@gmail.com>
2020-09-02 21:01:23 -0700

    update shopping list container

--- frontend/src/components/shopping_list/shopping_list_container.js
+++ frontend/src/components/shopping_list/shopping_list_container.js
@@ -1,14 +1,21 @@
 import { connect } from "react-redux";
 import ShoppingList from "./shopping_list";
+import {
+  removeShoppingListItem,
+  toggleItemDone
+} from "../../actions/shopping_list_item_actions";
 
 const mapStateToProps = (state) => {
   return {
-    
+    userId: state.session.user.id,
+    listItems: state.entities.shoppingListItems
   };
 };
 
 const mapDispatchToProps = (dispatch) => {
   return {
+    removeItem: (userId, itemId) => dispatch(removeShoppingListItem(userId, itemId)),
+    toggleItemDone: (userId, itemId) => dispatch(toggleItemDone(userId, itemId))
   };
 };
 
