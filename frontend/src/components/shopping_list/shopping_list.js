d77eec16d388875927ab3cc206acccf8ff084df9
Jack Fragassi <jfragassi98@gmail.com>
2020-09-03 10:52:48 -0700

    connect shopping list to backend

--- frontend/src/components/shopping_list/shopping_list.js
+++ frontend/src/components/shopping_list/shopping_list.js
@@ -18,18 +18,48 @@ class ShoppingList extends React.Component {
       this.props.history.push("/foods");
   }
 
+  componentDidMount() {
+    this.props.requestItems(this.props.userId);
+    this.props.getFridges(this.props.userId);
+  }
+
   render() {
+    console.log(this.props.listItems);
+    console.log(Object.keys(this.props.listItems));
+    if (Object.keys(this.props.listItems).length === 0) return null;
+    if (Object.keys(this.props.fridges).length === 0) return null;
     return (
       <div>
         <NavBarContainer/>
         <div className="shopping-list">
           <h1 className="shopping-list-header">Shopping List</h1>
           <div className="shopping-list-section">
-            <FontAwesomeIcon className="shopping-list-circle-icon" icon={faCircle} />
-            <div className="shopping-list-item-details">
-              <p className="shopping-list-item">Shopping List Item</p>
-              <p className="belongs-to-fridge">Fridge Name</p>
-            </div>
+            <ul className="shopping-list-categories">
+              {
+                Object.keys(this.props.listItems).map((category) => {
+                  return <li className="shopping-list-category">
+                    <span className="shopping-list-category-label">{category}</span>
+                    <ul className="shopping-list-category-items">
+                      {
+                        this.props.listItems[category].map((item) => {
+                          return <li className="shopping-list-item">
+                            <FontAwesomeIcon
+                              className="shopping-list-circle-icon" 
+                              icon={faCircle} />
+                            <div className="shopping-list-item-details">
+                              <p className="shopping-list-item-name">{item.name}</p>
+                              <p className="belongs-to-fridge">
+                                {this.props.fridges[item.fridgeId].name}
+                              </p>
+                            </div>
+                          </li>
+                        })
+                      }
+                    </ul>
+                  </li>
+                })
+              }
+            </ul>
           </div>
         </div>
         <div className="add-items-section">
--- frontend/src/components/shopping_list/shopping_list_container.js
+++ frontend/src/components/shopping_list/shopping_list_container.js
@@ -2,20 +2,28 @@ import { connect } from "react-redux";
 import ShoppingList from "./shopping_list";
 import {
   removeShoppingListItem,
-  toggleItemDone
+  toggleItemDone,
+  editShoppingListItemQuantity,
+  requestShoppingListItems
 } from "../../actions/shopping_list_item_actions";
+import { fetchUserFridges } from '../../actions/fridge_actions';
 
 const mapStateToProps = (state) => {
   return {
     userId: state.session.user.id,
-    listItems: state.entities.shoppingListItems
+    listItems: state.entities.shoppingListItems,
+    fridges: state.entities.fridges
   };
 };
 
 const mapDispatchToProps = (dispatch) => {
   return {
     removeItem: (userId, itemId) => dispatch(removeShoppingListItem(userId, itemId)),
-    toggleItemDone: (userId, itemId) => dispatch(toggleItemDone(userId, itemId))
+    toggleItemDone: (userId, itemId) => dispatch(toggleItemDone(userId, itemId)),
+    editQuantity: (userId, itemId, newQuant) =>
+      dispatch(editShoppingListItemQuantity(userId, { listItemId: itemId, quantity: newQuant })),
+    requestItems: (userId) => dispatch(requestShoppingListItems(userId)),
+    getFridges: (userId) => dispatch(fetchUserFridges(userId))
   };
 };
 
