0fb015ddf8a03d819077b755a348db703af935bb
anyahirota <anyahirota@gmail.com>
2020-09-03 15:20:34 -0700

    Start user search

---
+++ frontend/src/components/fridge/user_search_add.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faSearch,
+  faTimesCircle,
+  faPlus,
+} from "@fortawesome/free-solid-svg-icons";
+
+class UserSearchAndAdd extends React.Component {
+  
+
+  render() {
+
+  }
+}
+
+export default UserSearchAndAdd; 
---
+++ frontend/src/components/fridge/user_search_add_container.js
@@ -0,0 +1,2 @@
+import { connect } from "react-redux";
+import UserSearchAndAdd from "./user_search_add"; 
