1fbe8b524c26dc7f31286649a372edba44476535
anyahirota <anyahirota@gmail.com>
2020-09-02 21:54:56 -0700

    Create fridge list item skeleton

--- frontend/src/components/fridge/fridge.js
+++ frontend/src/components/fridge/fridge.js
@@ -1,6 +1,7 @@
 import React from "react";
 import "./fridge_css.scss";
 import NavBarContainer from "../nav/navbar_container";
+import FridgeItem from "./fridge_item"; 
 // import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 // import { } from "@fortawesome/free-solid-svg-icons";
 
@@ -16,22 +17,6 @@ class Fridge extends React.Component {
     this.props.history.push("/foods");
   }
 
-  daysUntilExp() {
-    const today = new Date();
-    const dd = String(today.getDate()).padStart(2, "0");
-    const mm = String(today.getMonth() + 1).padStart(2, "0"); 
-    const yyyy = today.getFullYear();
-
-    const currentDate = yyyy + "-" + mm + "-" + dd;
-    const expDate = this.props.expirationDate; // get item expiration date
-
-    const diffDays = (expirationDate, currentDate) =>
-      Math.ceil((expirationDate - currentDate) / (1000 * 60 * 60 * 24));
-
-    //will return the num of days until expiration as pos int, and days passed expiration as neg int
-    return diffDays(new Date(expDate), new Date(currentDate));  
-  }
-
   expiringItemsColor() {
     // this function will change the text-color based on how close the expiration date is:
     // 1 week or more $dark-blue
@@ -43,19 +28,29 @@ class Fridge extends React.Component {
     return (
       <div className="fridge-container">
         <NavBarContainer />
+
         <div className="fridge">
           <h1 className="fridge-name">Fridge Name</h1>
-          <div className="fridge-item-details">
-            <div className="fridge-left">
-              <p className="fridge-item-name">Fridge Item</p>
-              <p className="fridge-item-owner">Owner</p>
-            </div>
-            <div className="fridge-right">
-              <p className="fridge-item-ex">Expiration</p>
-              <p className="fridge-item-time">Days Left</p>
-            </div>
-          </div>
+          <ul>
+            <li className="fridge-item-details">
+              <div className="fridge-left">
+                <p className="fridge-item-name">Fridge Item</p>
+                <p className="fridge-item-owner">Owner</p>
+              </div>
+              <div className="fridge-right">
+                <p className="fridge-item-ex">Expiration</p>
+                <p className="fridge-item-time">Days Left</p>
+              </div>
+            </li>
+            {/* 
+            map over fridge items 
+
+            {this.props.fridgeItems.map((fridgeItem) => {
+                <FridgeItem fridgeItem={fridgeItem} />
+            })} */}
+          </ul>
         </div>
+
         <div className="add-items-section">
           <button onClick={this.redirectToAdd} className="add-items-btn">
             Add Items
---
+++ frontend/src/components/fridge/fridge_item.js
@@ -0,0 +1,40 @@
+import React from "react"; 
+
+class FridgeItem extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  daysUntilExp() {
+    const today = new Date();
+    const dd = String(today.getDate()).padStart(2, "0");
+    const mm = String(today.getMonth() + 1).padStart(2, "0");
+    const yyyy = today.getFullYear();
+
+    const currentDate = yyyy + "-" + mm + "-" + dd;
+    const expDate = this.props.expirationDate; // get item expiration date
+
+    const diffDays = (expirationDate, currentDate) =>
+      Math.ceil((expirationDate - currentDate) / (1000 * 60 * 60 * 24));
+
+    //will return the num of days until expiration as pos int, and days passed expiration as neg int
+    return diffDays(new Date(expDate), new Date(currentDate));
+  }
+
+  render() {
+    return (
+      <li className="fridge-item-details">
+        <div className="fridge-left">
+          <p className="fridge-item-name">{this.props.fridgeItem.name}</p>
+          <p className="fridge-item-owner">{this.props.fridgeItem.owner}</p>
+        </div>
+        <div className="fridge-right">
+          <p className="fridge-item-ex">Expiration</p>
+          <p className="fridge-item-time">Days Left</p>
+        </div>
+      </li>
+    );
+  }
+}
+
+export default FridgeItem; 
