258967b5c2366d8c72e2b3835b65882bee8a82c2
Jack Fragassi <jfragassi98@gmail.com>
2020-09-03 11:55:40 -0700

    Persist entities across browser refreshes

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -15,12 +15,20 @@ import { setAuthToken } from "./util/session_api_util";
 
 // We have not created this action yet, but will do so in the next step
 import { logout } from "./actions/session_actions";
+
+// For persisting state across browser refreshes
+import { loadState, saveState } from './local_storage';
+import throttle from 'lodash/throttle';
+
 import * as fridgeItemsActions from './actions/fridge_items_actions';
 import * as fridgeActions from './actions/fridge_actions';
 import * as shoppingListActions from './actions/shopping_list_item_actions';
 
 document.addEventListener("DOMContentLoaded", () => {
   let store;
+
+  // load persisted entities from localStorage
+  const persistedState = loadState();
 
   // If a returning user has a session token stored in localStorage
   if (localStorage.jwtToken) {
@@ -35,7 +43,12 @@ document.addEventListener("DOMContentLoaded", () => {
       session: { isAuthenticated: true, user: decodedUser },
     };
 
-    store = configureStore(preloadedState);
+    if (persistedState) {
+      persistedState.session = preloadedState.session;
+      store = configureStore(persistedState);
+    } else {
+      store = configureStore(preloadedState);
+    }
 
     const currentTime = Date.now() / 1000;
 
@@ -46,9 +59,19 @@ document.addEventListener("DOMContentLoaded", () => {
       window.location.href = "/login";
     }
   } else {
-    // If this is a first time user, start with an empty store
-    store = configureStore({});
+    // If this is a first time user, start with just persisted state
+    if (persistedState) {
+      store = configureStore(persistedState);
+    } else {
+      store = configureStore({});
+    }
   }
+
+  store.subscribe(throttle(() => {
+    saveState({
+      entities: store.getState().entities
+    });
+  }, 1000));
 
   //FOR TESTING
   window.getState = store.getState;
