4c80ee8b0beed8b0b6d8e14672abefcd1db96666
anyahirota <anyahirota@gmail.com>
2020-09-03 12:06:10 -0700

    Add food component actions and fridge assignment

--- frontend/src/components/food_items/food_item_modal_container.js
+++ frontend/src/components/food_items/food_item_modal_container.js
@@ -1,5 +1,8 @@
 import { connect } from "react-redux";
 import FoodItemModalForm from "./food_item_modal_form"; 
+import {addFridgeItem} from "../../actions/fridge_items_actions"; 
+import { addShoppingListItem } from "../../actions/shopping_list_item_actions"; 
+import { fetchUserFridges } from "../../actions/fridge_actions"; 
 import { withRouter } from "react-router-dom";
 
 //need user patch and fridge patch - add items to either place 
@@ -9,12 +12,18 @@ const mapStateToProps = (state, ownProps) => {
     name: ownProps.name,
     category: ownProps.category,
     imageUrl: ownProps.imageUrl,
-    fridge: "all the fridges owned by user",
+    fridges: Object.values(state.entities.fridges),
     userId: state.session.user.id,
   };
 };
 
 const mapDispatchToProps = (dispatch) => ({
+  fetchUserFridges: (userId) => 
+    dispatch(fetchUserFridges(userId)), 
+  addFridgeItem: (fridgeId, itemData) =>
+    dispatch(addFridgeItem(fridgeId, itemData)),
+  addShoppingListItem: (userId, itemData) =>
+    dispatch(addShoppingListItem(userId, itemData)),
 });
 
 export default withRouter (connect(
