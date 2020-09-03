f1b4662416e4f608786b5e3e7e5d727047eef13c
henryhannn <henryhan3996@gmail.com>
2020-09-03 10:04:03 -0700

    add fridge container

--- frontend/src/components/fridge/fridge_container.js
+++ frontend/src/components/fridge/fridge_container.js
@@ -1,13 +1,30 @@
 import { connect } from "react-redux";
-
+import {
+  receiveFridge,
+  removeFridge,
+  fetchUserFridges,
+  createFridge,
+  deleteFridge
+} from "../../actions/fridge_actions";
 import Fridge from "./fridge";
 
-const mapStateToProps = (state) => ({
-  // fridgeContainer: Object.values(state.entities.fridgeItems),
-});
+const mapStateToProps = (state) => {
+  return {
+    fridgeId: state.fridge.id,
+    name: state.fridge.name,
+    participants: state.fridge.participants,
+    fridgeItems: state.entities.fridgeItems
+  };
+};
 
-const mapDispatchToProps = (dispatch) => ({
-  
-});
+const mapDispatchToProps = (dispatch) => {
+  return {
+    receiveFridge: (fridge) => dispatch(receiveFridge(fridge)),
+    removeFridge: (fridge) => dispatch(removeFridge(fridge)),
+    fetchUserFridges: (userId) => dispatch(fetchUserFridges(userId)),
+    createFridge: (userId, name) => dispatch(createFridge(userId, name)),
+    deleteFridge: (fridgeId) => dispatch(deleteFridge(fridgeId))
+  };
+};
 
 export default connect(mapStateToProps, mapDispatchToProps)(Fridge);
